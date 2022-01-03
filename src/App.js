fa9659e18ac73f350e19dad2b9331c793954ff10
Max Henry <maxhenry@maxs-mbp-2.lan>
2022-01-02 19:11:56 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
+import { useRef, useState, useEffect } from 'react';
 import './App.css';
 
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tf from '@tensorflow/tfjs-core';
+import '@tensorflow/tfjs-backend-webgl';
+import Webcam from 'react-webcam';
+
+import calculateHeadRotation from './utilities';
+
 function App() {
+  const INFERENCES_PER_REFRESH_INTERVAL = 1;
+  const PREDICTION_INTERVAL_MS = 200;
+  const webcamRef = useRef(null);
+
+  const runMoveNet = async () => {
+    const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER };
+    const detector = await poseDetection.createDetector(
+      poseDetection.SupportedModels.MoveNet, detectorConfig
+    );
+
+    setInterval(() => {
+      detect(detector);
+    }, PREDICTION_INTERVAL_MS);
+  };
+
+  const detect = async (detector) => {
+    const isCamDefined = () => typeof webcamRef.current !== "undefined";
+    const isCamNull = () => webcamRef.current == null;
+    const isCamReady = () => webcamRef.current.video.readyState === 4;
+
+    if (isCamDefined() && !isCamNull() && isCamReady()) {
+      const video = webcamRef.current.video;
+      const pose = await detector.estimatePoses(video);
+
+      const isKeypointsDefined = () => typeof pose[0].keypoints !== "undefined";
+
+      if (isKeypointsDefined()) {
+        const tmp = calculateHeadRotation(pose);
+        console.log(tmp);
+      }
+    }
+  }
+
+  runMoveNet();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <p>Head rotation: {rotation.toFixed(4)}</p> */}
+        <Webcam ref={webcamRef} />
       </header>
     </div>
   );
---
+++ src/utilities.js
@@ -0,0 +1,41 @@
+export default function calculateHeadRotation(pose){
+
+  let nose = pose[0].keypoints[0];
+  let leftEye = pose[0].keypoints[1];
+  let rightEye = pose[0].keypoints[2];
+
+  // Set left eye as origin.
+  rightEye = subtractKeypoint(rightEye, leftEye);
+  nose = subtractKeypoint(nose, leftEye);
+
+  // Rotate so rightEye is flat on X-axis.
+  const angle = findAngle(rightEye);
+  rightEye = rotateKeypoint(rightEye, -angle);
+  nose = rotateKeypoint(nose, -angle);
+
+  // Express nose location as normalized [-1, 1] between eyes.
+  let headRotation = (nose.x / rightEye.x) - 0.5;
+
+  return headRotation;
+}
+
+function subtractKeypoint(minuend, subtrahend) {
+  minuend.x -= subtrahend.x;
+  minuend.y -= subtrahend.y;
+
+  return minuend;
+}
+
+function findAngle(keypoint) {
+  return Math.atan(keypoint.y / keypoint.x);
+}
+
+function rotateKeypoint(keypoint, angle) {
+  const x = keypoint.x;
+  const y = keypoint.y;
+
+  keypoint.x = Math.cos(angle) * x - Math.sin(y);
+  keypoint.y = Math.sin(angle) * x + Math.cos(y);
+
+  return keypoint;
+}
