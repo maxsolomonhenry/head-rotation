1569c2aef4ede73c17e9bffcbc85991a10f48e6c
Max Henry <max.henry@mail.mcgill.ca>
2023-02-18 20:09:04 -0500

    WIP refactoring experiment

--- matlab/experiment.m
+++ matlab/experiment.m
@@ -22,39 +22,35 @@ generator = StimulusGenerator(numChannels, burstDurSecs, stepDurSecs, numRhythmR
 whichExperiments = ["source", "combined"];
 allNumTrialRepeats = [4, 4];
 
-f = waitbar(0, sprintf("Experiment %d...", i));
-for i = 1:length(whichExperiments)
+df = makeStimulusTable( ...
+    whichExperiments(i), ...
+    allNumTrialRepeats(i));
 
-    df = makeStimulusTable( ...
-        whichExperiments(i), ...
-        allNumTrialRepeats(i));
+waitbar(0, f, "Progress...");
+for j = 1:height(df)
 
-    waitbar(0, f, sprintf("Experiment %d...", i));
-    for j = 1:height(df)
-
-        if j > NUMDEBUG
-            break;
-        end
-
-        progress = j / height(df);
-        waitbar(progress, f, sprintf(sprintf("Experiment %d...", i)));
-
-        trial = df(j, :);
-        outputs = trial.outputs;
-        isRhythmic = trial.isRhythmic;
-
-        x = db2mag(gainDb) * generator.make(outputs, isRhythmic);
-        
-        player.play(x);
-
-        response = inputdlg("Which direction (l/r/f/b): ");
-
+    if j > NUMDEBUG
+        break;
     end
 
-    waitbar(1, f, "Experiment complete. Press any key to continue...");
-    pause();
+    progress = j / height(df);
+    waitbar(progress, f, "Progress...");
+
+    trial = df(j, :);
+    outputs = trial.outputs;
+    isRhythmic = trial.isRhythmic;
+
+    x = db2mag(gainDb) * generator.make(outputs, isRhythmic);
+    
+    player.play(x);
+
+    response = inputdlg("Which direction (l/r/f/b): ");
 
 end
+
+waitbar(1, f, "Experiment complete. Press any key to continue...");
+pause();
+
 
 waitbar(1, f, "Done! Press any key to close...");
 pause();
