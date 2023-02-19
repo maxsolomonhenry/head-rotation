6c714344c12a20c388c87670dc9527735b4f1b2a
Max Henry <max.henry@mail.mcgill.ca>
2023-02-18 20:22:26 -0500

    WIP making save data thing

--- matlab/experiment.m
+++ matlab/experiment.m
@@ -2,7 +2,9 @@ clc; clear; close all;
 
 addpath(genpath('.'));
 
-NUMDEBUG = 20;
+NUMDEBUG = 2;
+
+datadir = "data/";
 
 whichDevice = 'Head rotation';
 sr = 48000;
@@ -20,37 +22,49 @@ player = Player(whichDevice, blockSize, sr);
 generator = StimulusGenerator(numChannels, burstDurSecs, stepDurSecs, numRhythmRepeats, sr);
 
 whichExperiments = ["source", "combined"];
-allNumTrialRepeats = [4, 4];
 
-df = makeStimulusTable( ...
-    whichExperiments(i), ...
-    allNumTrialRepeats(i));
+partipantId = inputdlg("Participant ID:");
 
-waitbar(0, f, "Progress...");
-for j = 1:height(df)
+dateTime = datestr(now, 'yymmdd_HHMM');
 
-    if j > NUMDEBUG
-        break;
+% Create the file name
+fileName = ['PID_', participantId, '_', dateTime, '.txt'];
+
+fpath = 
+
+f = waitbar(0, sprintf("Experiment %d...", i));
+for i = 1:length(whichExperiments)
+
+    df = makeStimulusTable(whichExperiments(i), numTrialRepeats);
+
+    waitbar(0, f, sprintf("Experiment %d...", i));
+    for j = 1:height(df)
+
+        if j > NUMDEBUG
+            break;
+        end
+
+        progress = j / height(df);
+        waitbar(progress, f, sprintf(sprintf("Experiment %d...", i)));
+
+        trial = df(j, :);
+        outputs = trial.outputs;
+        isRhythmic = trial.isRhythmic;
+        trialId = trial.trialId;
+
+        x = db2mag(gainDb) * generator.make(outputs, isRhythmic);
+        
+        player.play(x);
+
+        response1 = inputdlg("Which direction (l/r/f/b) (fl/fr/bl/br)?: ");
+        response2 = inputdlb("Was that headphones or speakers (h/s)?: ");
+
     end
 
-    progress = j / height(df);
-    waitbar(progress, f, "Progress...");
-
-    trial = df(j, :);
-    outputs = trial.outputs;
-    isRhythmic = trial.isRhythmic;
-
-    x = db2mag(gainDb) * generator.make(outputs, isRhythmic);
-    
-    player.play(x);
-
-    response = inputdlg("Which direction (l/r/f/b): ");
+    waitbar(1, f, "Experiment complete. Press any key to continue...");
+    pause();
 
 end
-
-waitbar(1, f, "Experiment complete. Press any key to continue...");
-pause();
-
 
 waitbar(1, f, "Done! Press any key to close...");
 pause();
